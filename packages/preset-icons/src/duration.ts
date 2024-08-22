14ed47c7bbc5a4a806dc164d18dcb4bbe7d011c7
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2024-08-22 10:27:41 +0800

    chore: precision

--- packages/preset-icons/src/duration.ts
+++ packages/preset-icons/src/duration.ts
@@ -2,10 +2,10 @@ const ANIMATE_ELEMENTS = ['animate', 'animateMotion', 'animateTransform', 'set']
 
 const TIMING_ATTRIBUTES = ['begin', 'dur']
 
-export function scaleDuration(svg: string, scale: number) {
+export function scaleDuration(svg: string, scale: number, precision = 100) {
   return ANIMATE_ELEMENTS.reduce((_svg, el) => {
     const reg = new RegExp(`(?<=<${el}.*?(?:${TIMING_ATTRIBUTES.join('|')})=".*?)(\\d+(?:\\.\\d+)?(?=s))`, 'gi')
-    _svg = _svg.replace(reg, (duration: string) => (+duration * scale).toString())
+    _svg = _svg.replace(reg, (duration: string) => (Math.ceil(+duration * scale * precision) / precision).toString())
     return _svg
   }, svg)
 }
